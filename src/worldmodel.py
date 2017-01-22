e1bcf4057119cf2f63e401b089b7a6ca0ad8313f
Arthur O'Dwyer <arthur.j.odwyer@gmail.com>
2017-01-21 21:47:39 -0800

    Replace pickle-based store with sqlite3-based store.
    No change to the front-end.

--- src/worldmodel.py
+++ src/worldmodel.py
@@ -1,5 +1,5 @@
 import os
-import pickle
+import sqlite3
 import uuid
 
 _places = {}
@@ -12,8 +12,10 @@ def get_root_num():
     return 'home'
 
 class Place:
-    def __init__(self, longdesc):
-        self.num = get_random_num()
+    def __init__(self, num, longdesc):
+        if num is None:
+            num = get_random_num()
+        self.num = num
         self.longdesc = longdesc
     def __str__(self):
         raise ValueError()
@@ -22,36 +24,45 @@ class Place:
         return [c for c in _connections.values() if c.predecessor_num == self.num]
 
 class Connection:
-    def __init__(self, predecessor_num, how, successor_num):
-        self.num = get_random_num()
+    def __init__(self, num, predecessor_num, how, successor_num):
+        if num is None:
+            num = get_random_num()
+        self.num = num
         self.predecessor_num = predecessor_num
         self.how = how
         self.successor_num = successor_num
     def __str__(self):
         raise ValueError()
 
-def load_data(force=False):
+def init_data():
+    this_dir = os.path.abspath(os.path.dirname(__file__))
+    with sqlite3.connect(os.path.join(this_dir, 'data.db')) as c:
+        c.execute('CREATE TABLE places (num TEXT PRIMARY KEY, longdesc TEXT)')
+        c.execute('CREATE TABLE connections (num TEXT PRIMARY KEY, how TEXT, predecessor_num TEXT, successor_num TEXT)')
+
+def load_data():
     global _places, _connections
     print 'thinking about loading data...'
     if not _places:
         print 'loading data...'
         try:
             this_dir = os.path.abspath(os.path.dirname(__file__))
-            with open(os.path.join(this_dir, 'data.pickle'), 'r') as f:
-                data = pickle.load(f)
+            with sqlite3.connect(os.path.join(this_dir, 'data.db')) as c:
+                for row in c.execute('SELECT * FROM places'):
+                    _places[row[0]] = Place(row[0], row[1])
+                for row in c.execute('SELECT * FROM connections'):
+                    _connections[row[0]] = Connection(row[0], row[1], row[2], row[3])
         except Exception:
-            data = ({}, {})
-        (new_places, new_connections) = data
-        _places.update(new_places)
-        _connections.update(new_connections)
+            init_data()
 
 def store_data():
     global _places, _connections
     this_dir = os.path.abspath(os.path.dirname(__file__))
-    data = (_places, _connections)
-    with open(os.path.join(this_dir, 'data2.pickle'), 'w') as f:
-        pickle.dump(data, f)
-    os.rename(os.path.join(this_dir, 'data2.pickle'), os.path.join(this_dir, 'data.pickle'))
+    with sqlite3.connect(os.path.join(this_dir, 'data.db')) as c:
+        for p in _places.values():
+            c.execute('INSERT INTO places VALUES (?,?)', (p.num, p.longdesc))
+        for p in _connections.values():
+            c.execute('INSERT INTO connections VALUES (?,?,?,?)', (p.num, p.predecessor_num, p.how, p.successor_num))
 
 def get_default_place():
     global _places
@@ -67,12 +78,12 @@ def get_place(num):
 
 def create_place(predecessor_num, how, longdesc):
     global _places, _connections
-    place = Place(longdesc)
+    place = Place(None, longdesc)
     if predecessor_num is None:
         place.num = get_root_num()
     _places[place.num] = place
     if predecessor_num is not None:
-        connection = Connection(predecessor_num, how, place.num)
+        connection = Connection(None, predecessor_num, how, place.num)
         _connections[connection.num] = connection
     store_data()
     return place.num
